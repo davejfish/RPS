9a7e4d957a68110b30f410cb993e7908e521a52f
Dave Fisher <dave.j.fish@gmail.com>
2022-06-07 17:51:20 -0700

    name input updates player name across all sections

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // import needed modules
-
+// import { score, getRandomItem } from './utils.js';
 // state
+
+
 
 // components
     // component
@@ -12,6 +14,17 @@
         // state update
         // re-display components (which ones?)
     // optional: handle functions for shared event handler logic
+
+
+// updating name input across every section
+let nameInput = document.getElementById('nameInput');
+let nameDisplay = document.querySelectorAll('[id=name-display]');
+
+nameInput.addEventListener('change', () => {
+    for (let i of nameDisplay) {
+        i.textContent = nameInput.value;
+    }
+});
 
 // page load actions
 
